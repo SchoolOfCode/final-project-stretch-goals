a0a6345f36bf00b7d62dde8e0e3c7805699d1c6d
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-05-06 14:05:56 +0100

    changed jpg

--- client/components/HowItWorks/index.js
+++ client/components/HowItWorks/index.js
@@ -2,9 +2,9 @@ import css from "./HowItWorks.module.css";
 import arrow from "./StepDisplay/right-arrow.svg";
 import StepDisplay from "./StepDisplay/index";
 import image1 from "../../images/step1.png";
-// import image2 from "../../images/step2.JPG";
-// import image3 from "../../images/step3.JPG";
-//import image4 from "../../images/step4.JPG";
+import image2 from "../../images/step2.jpg";
+import image3 from "../../images/step3.jpg";
+import image4 from "../../images/step4.jpg";
 
 export default function HowItWorks() {
   const steps = [
@@ -17,21 +17,21 @@ export default function HowItWorks() {
     },
     {
       classTitle: "two",
-      image: "image4",
+      image: image2,
       head: "Book your lesson",
       text:
         "Choose a lesson length, time, and day that suits you using our at-a-glance calendar. When your tutor confirms the lesson is on, the lesson appears in your calendar so you won't forget."
     },
     {
       classTitle: "three",
-      image: "image4",
+      image: image3,
       head: "Learn with us",
       text:
         "Start your learning journey with your tutor using the agreed video call software."
     },
     {
       classTitle: "four",
-      image: "image4",
+      image: image4,
       head: "Leave a review",
       text:
         "After each lesson, both student and tutor can leave a rating and review of the lesson. This keeps our community standards high."
