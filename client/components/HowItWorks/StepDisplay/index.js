f4383636e33c27447b6679ab4a260add0502350a
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-05-06 14:08:52 +0100

    1

--- client/components/HowItWorks/StepDisplay/index.js
+++ client/components/HowItWorks/StepDisplay/index.js
@@ -6,7 +6,7 @@ export default function StepDisplay({ text, image, head }) {
       <div className={css.head}>
         <h3>{head}</h3>
       </div>
-      <img src={image} className={css.imageContainer} />
+      <img src={require(image)} className={css.imageContainer} />
       <div className={css.textDiv}>{text}</div>
     </div>
   );
