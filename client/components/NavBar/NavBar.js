0762fc6a44e013e4608f9f80493433110dcc96ef
marksgilligan <59980448+marksgilligan@users.noreply.github.com>
2020-05-08 06:12:55 +0100

    Removed old login window

--- client/components/NavBar/NavBar.js
+++ client/components/NavBar/NavBar.js
@@ -2,37 +2,13 @@ import css from "./NavBar.module.css";
 import Button from "../Button/Button";
 import Link from "next/link";
 import { useState } from "react";
-import LoginWindow from "../LoginWindow/LoginWindow";
 import Cognito from "../Cognito/Cognito";
 const AWS = require("aws-sdk");
 import { useAuth } from "../../auth";
 
 export default function NavBar() {
-  const [displayLogin, setDisplayLogin] = useState(false);
   const auth = useAuth();
   console.log("damn daaaayyyyniel", auth ? auth.idTokenData.sub : null);
-  // console.log(
-  //   "1) User Attributes",
-  //   event.request.userAttributes
-  //     ? event.request.userAttributes
-  //     : "it's undefined"
-  // );
-  // console.log("2) idTokenData.email", idTokenData.email);
-  // console.log("3) Object.idTokenData.email", Object.idTokenData.email);
-  // console.log("3.5) object.idTokenData.email", object.idTokenData.email);
-  // console.log("4)  idTokenData", idTokenData);
-  // console.log("5)  Object.idTokenData", Object.idTokenData);
-  // var identityId = AWS.config.credentials;
-  // console.log("id = ", identityId);
-
-  //  console.log(AWS.CognitoIdentityCredentials);
-
-  // var tokenTest = Object(I.useAuth)(e.initialAuth);
-  // console.log("token", tokenTest);
-
-  function toggleLogin() {
-    setDisplayLogin(!displayLogin);
-  }
 
   return (
     <div className={css.navBarContainer}>
@@ -44,32 +20,12 @@ export default function NavBar() {
 
       <div className={css.loginButton}>
         <Cognito />
-
-        {/* 
-        OLD LOGIN BUTTON
-        
-        <Button
-          className={css.actualButton}
-          text="Login / Create Account"
-          handleClick={toggleLogin}
-        /> */}
       </div>
       <Link href="/search">
         <div className={css.searchField}>
           <Button text="Search Tutors" />
         </div>
       </Link>
-      {displayLogin && (
-        <div
-          style={{
-            position: "absolute",
-            paddingLeft: "25vw",
-            paddingTop: "20vh",
-          }}
-        >
-          <LoginWindow setDisplayLogin={setDisplayLogin} />
-        </div>
-      )}
     </div>
   );
 }
