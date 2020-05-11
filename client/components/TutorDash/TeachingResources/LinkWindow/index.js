7262cc7834e1454ada136d6b3ebaec0e7adc5930
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-05-11 19:56:15 +0100

    css changes made to navbar and all components

--- client/components/TutorDash/TeachingResources/LinkWindow/index.js
+++ client/components/TutorDash/TeachingResources/LinkWindow/index.js
@@ -6,7 +6,8 @@ export default function LinkWindow({ logo, resource, url }) {
       <a href={url}>
         <img src={logo} className={css.imageContainer}></img>
       </a>
-      <p>{resource}</p>
+      <p className={css.resource}>{resource}</p>
+      <p className={css.text}>lorem ipsum woop woop wop wopp wop</p>
     </div>
   );
 }
